60d53a5f474fa75efc8d669375e14eeb93ed4a25
masterUNG <phrombutr@gmail.com>
2016-11-16 13:03:34 +0700

    Check Space finish

--- app/src/main/java/com/example/n56j/project1/ActivityRegisterAdmin.java
+++ app/src/main/java/com/example/n56j/project1/ActivityRegisterAdmin.java
@@ -1,18 +1,61 @@
 package com.example.n56j.project1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ActivityRegisterAdmin extends AppCompatActivity {
 
     //Explicit
     private String urlAddUserString = "http://swiftcodingthai.com/pbru/add_user_master.php";
-
+    private EditText userEditText, passwordEditText, phoneEditText, emailEditText;
+    private String userString, passwordString, phoneString, emailString;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_register_admin);
+
+        //Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText10);
+        passwordEditText = (EditText) findViewById(R.id.editText11);
+        phoneEditText = (EditText) findViewById(R.id.editText15);
+        emailEditText = (EditText) findViewById(R.id.editText14);
+        button = (Button) findViewById(R.id.button7);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From Edit Text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+                phoneString = phoneEditText.getText().toString().trim();
+                emailString = emailEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("") ||
+                        phoneString.equals("") || emailString.equals("")) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(ActivityRegisterAdmin.this,
+                            R.drawable.doremon48,
+                            getResources().getString(R.string.title_haveSpace),
+                            getResources().getString(R.string.message_haveSpace));
+                    myAlert.myDialog();
+
+                } else {
+                    //No Space
+
+                }
+
+            }   // onClick
+        });
+
+
     }   // Main Method
 
 }   // Main Class

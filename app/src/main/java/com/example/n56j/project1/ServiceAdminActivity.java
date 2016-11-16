f91310d1ac0e6de7dc36b893f3434e5cb71dc981
masterUNG <phrombutr@gmail.com>
2016-11-16 16:56:48 +0700

    Finish 16nov

--- app/src/main/java/com/example/n56j/project1/ServiceAdminActivity.java
+++ app/src/main/java/com/example/n56j/project1/ServiceAdminActivity.java
@@ -1,7 +1,10 @@
 package com.example.n56j.project1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,11 +18,30 @@ public class ServiceAdminActivity extends FragmentActivity implements OnMapReady
     private GoogleMap mMap;
     private double latPbruADouble = 13.071468;
     private double lngPbruADouble = 99.976380;
+    private Button listViewButton, addMarkerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_service_layout);
+
+        //Bind Widget
+        listViewButton = (Button) findViewById(R.id.button15);
+        addMarkerButton = (Button) findViewById(R.id.button16);
+
+        //addMarker Controller
+        addMarkerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ServiceAdminActivity.this, AddEditActivity.class);
+                intent.putExtra("Status", true);
+                startActivity(intent);
+
+            }   // onClick
+        });
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);

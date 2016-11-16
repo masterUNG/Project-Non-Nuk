d5a2beeed516fa55285734206eb27da39077e8c7
masterUNG <phrombutr@gmail.com>
2016-11-16 15:44:38 +0700

    Authen Admin Success

--- app/src/main/java/com/example/n56j/project1/ActivityAdmin.java
+++ app/src/main/java/com/example/n56j/project1/ActivityAdmin.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ActivityAdmin extends AppCompatActivity {
 
     //Explicit
     private EditText userEditText, passwordEditText;
-    private String userString, passwordString;
+    private String userString, passwordString, truePasswordString;
     private String urlJSON = "http://swiftcodingthai.com/pbru/get_user_where_master.php";
+    private boolean aBoolean = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +59,42 @@ public class ActivityAdmin extends AppCompatActivity {
 
                 String strJSON = getUser.get();
                 Log.d("16novV2", "JSON ==> " + strJSON);
+
+                JSONArray jsonArray = new JSONArray(strJSON);
+
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    if (userString.equals(jsonObject.getString("User"))) {
+
+                        aBoolean = false;
+                        truePasswordString = jsonObject.getString("Password");
+
+                    }   // if
+                }   // for
+
+                if (aBoolean) {
+
+                    //User False
+                    MyAlert myAlert = new MyAlert(ActivityAdmin.this,
+                            R.drawable.bird48,
+                            getResources().getString(R.string.title_userFalse),
+                            getResources().getString(R.string.message_userFalse));
+                    myAlert.myDialog();
+
+                } else if (passwordString.equals(truePasswordString)) {
+                    //Password True
+                    Toast.makeText(ActivityAdmin.this,
+                            "Welcome", Toast.LENGTH_SHORT).show();
+                } else {
+                    //Password False
+                    MyAlert myAlert = new MyAlert(ActivityAdmin.this,
+                            R.drawable.kon48,
+                            getResources().getString(R.string.title_passwordFalse),
+                            getResources().getString(R.string.message_passwordFalse));
+                    myAlert.myDialog();
+                }
 
 
             } catch (Exception e) {
